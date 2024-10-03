ad8344bc1e2f0e436e371c3cc0b20a3f7941310d
Somiyo <somiyomagar@gmail.com>
2024-10-03 10:29:23 -0600

    storing data into database using mongoose model

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
-const mongoURI =
-  'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI).then(()=>console.log("Connected to Mongo Succesfully")).catch((e)=>console.log(e.message))
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log('Connected to Mongo Succesfully'))
+    .catch((e) => console.log(e.message))
 }
 
 module.exports = connectToMongo
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const connectToMongo = require('./db')
 const express = require('express')
-
-connectToMongo()
 const app = express()
 const port = 3000
 
+connectToMongo()
+app.use(express.json())
+
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
