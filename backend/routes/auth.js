b418e79686ea8797268aa4ba232d5c29899db5bf
Somiyo <somiyomagar@gmail.com>
2024-10-03 23:40:11 -0600

    added password hasing using bcryptjs in NodeJs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'Harry'
 
 // create a user using: POST "/api/auth/createUser" . No login required
 router.post(
@@ -28,12 +32,24 @@ router.post(
           .status(400)
           .json({ error: 'Sorry this email is used already' })
       }
+      const salt = await bcrypt.genSalt(10)
+      const secPass = await bcrypt.hash(req.body.password, salt) 
+
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       })
-      res.json(user)
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET)
+      res.json({authToken})
+      // res.json(user)
+
+
     } catch (error) {
       console.error(error.message)
       res.status(500).send('some error occured')
