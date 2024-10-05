2ea656aba545f45d82c86db5c80919c002572e9c
Somiyo <somiyomagar@gmail.com>
2024-10-05 14:32:33 -0600

    added property to fetch notes from Context

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,8 +1,23 @@
+import { useState } from 'react'
 import NoteContext from './noteContext'
 
 const NoteState = (props) => {
+  const notesInitial = [
+    {
+      "_id": "6700857233d7e3f645cac3ed",
+      "user": "670084b133d7e3f645cac3da",
+      "title": "My title",
+      "description": "finish the react app course",
+      "tag": "personal",
+      "date": "2024-10-05T00:16:50.852Z",
+      "__v": 0
+    }
+  ]
+
+ const [notes, setNotes] = useState(notesInitial)
+
    return (
-    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{notes, setNotes}}>{props.children}</NoteContext.Provider>
   )
 }
 
