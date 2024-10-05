c38b87f44f2e3a9e11ab8555802c9f53dc39728f
Somiyo <somiyomagar@gmail.com>
2024-10-05 11:45:49 -0600

    created navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBars'
+import Home from './components/Hoome'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
---
+++ src/components/Hoome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hoome = () => {
+  return (
+    <div>This is my home</div>
+  )
+}
+
+export default Hoome
