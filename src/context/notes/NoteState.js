9b8f338db802871cc5c76dcecd45542e4f8be7f4
Somiyo <somiyomagar@gmail.com>
2024-10-05 14:02:09 -0600

    added to make link active using useLocaton Hook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,8 @@
-import { useState } from 'react'
 import NoteContext from './noteContext'
 
 const NoteState = (props) => {
-  const s1 = {
-    name: 'Somiyo',
-    class: '5b',
-  }
-  const [state, setState] = useState(s1)
-  const update = () => {
-    setTimeout(() => {
-        setState({
-            name: 'Sajjan',
-            class: '5c',
-          })
-    }, 2000)
-  }
    return (
-    <NoteContext.Provider value={{state, update}}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   )
 }
 
