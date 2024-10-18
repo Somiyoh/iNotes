30629aac10421fb074826719a71b6fea05b05f09
Somiyo <somiyomagar@gmail.com>
2024-10-17 21:26:49 -0600

    created login and signup for users

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Hoome'
 import About from './components/About'
 import NoteState from './context/notes/NoteState'
 import { Alert } from './components/Alert'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
       <NoteState>
         <Router>
           <NavBar />
-          <Alert message="some message"/>
+          <Alert message="some message" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/login" element={<Login />} />
             </Routes>
           </div>
         </Router>
--- src/components/NavBars.js
+++ src/components/NavBars.js
@@ -24,12 +24,23 @@ const NavBars = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className={`nav-link ${location.pathname==="/" ? "active" : " "}`}aria-current="page" to="/">
+              <Link
+                className={`nav-link ${
+                  location.pathname === '/' ? 'active' : ' '
+                }`}
+                aria-current="page"
+                to="/"
+              >
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className={`nav-link ${location.pathname==="/about" ? "active" : " "}`} to="/about">
+              <Link
+                className={`nav-link ${
+                  location.pathname === '/about' ? 'active' : ' '
+                }`}
+                to="/about"
+              >
                 About
               </Link>
             </li>
@@ -41,9 +52,12 @@ const NavBars = () => {
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
+            <Link class="btn btn-primary mx-1" to="/login" role="button">
+              Login
+            </Link>
+            <Link class="btn btn-primary mx-1" to="/signup" role="button">
+              Signup
+            </Link>
           </form>
         </div>
       </div>
